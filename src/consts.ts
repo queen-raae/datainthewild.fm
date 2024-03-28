6558e497b7ffaed3ceaee4fa122c65dbd7c8e5dd
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-03-28 22:11:31 +0530

    remove utility classes

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,7 @@ export const SITE_TITLE = "Data in the Wild";
 export const SITE_DESCRIPTION = "";
 
 export const SHOW_TITLE =
-  '<span class="font-black font-neo text-primary">DATA</span> <span class="font-apricot">in the Wild</span>';
+  '<em>DATA</em> <span>in the Wild</span>';
 
 // export const SHOW_DESCRIPTION =
 //   "A podcast where your favorite indie hackers share their data model journey so you can learn from their experience, both good and bad!";
