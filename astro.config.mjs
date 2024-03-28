6a0856206ec15af2b61f04c1e40c5fc6ccb8cca3
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-03-28 17:28:09 +0530

    Use brand fonts in title

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
+import netlify from "@astrojs/netlify";
 import { defineConfig } from "astro/config";
 
-import netlify from "@astrojs/netlify";
-
-// https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: netlify(),
--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,12 @@
 export const SITE_TITLE = "Data in the Wild";
+
 export const SITE_DESCRIPTION = "";
 
-export const SHOW_TITLE = "Data in the <em>Wild</em>";
+export const SHOW_TITLE =
+  '<span class="font-black font-neo text-primary">DATA</span> <span class="font-apricot">in the Wild</span>';
+
 // export const SHOW_DESCRIPTION =
 //   "A podcast where your favorite indie hackers share their data model journey so you can learn from their experience, both good and bad!";
+
 export const SHOW_DESCRIPTION =
   "Learn from your favorite indie hackers as they share stories and tall tales from their data model journey!";
