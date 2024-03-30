8189b12187d0b0770cada9fb89814a235fcdb877
raae <queen@raae.codes>
2024-03-30 21:47:53 +0000

    fix: copy

--- src/consts.ts
+++ src/consts.ts
@@ -2,9 +2,9 @@ export const SITE_TITLE =
   "Data in the Wild - A Data Model Podcast brought to you by Xata";
 
 export const SITE_DESCRIPTION =
-  "A podcast where your favorite indie hackers share their hard earned lessons and tall tales from their data model journey!";
+  "A podcast where your favorite indie hackers share their hard-earned lessons and tall tales from their data model journeys!";
 
 export const SHOW_TITLE = "<strong>DATA</strong> <em>in the Wild</em>";
 
 export const SHOW_DESCRIPTION =
-  "Learn from your favorite indie hackers as they share hard earned lessons and <em>tall tales</em> from their data model journey!";
+  "Learn from your favorite indie hackers as they share hard-earned lessons and <em>tall tales</em> from their data model journeys!";
