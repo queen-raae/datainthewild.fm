565905bc5b4d4349a45424676bc09886414b5033
raae <queen@raae.codes>
2024-03-30 11:41:27 +0000

    feat: meta + og image

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import netlify from "@astrojs/netlify";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: "https://datainthewild.fm",
   output: "static",
   adapter: netlify(),
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,12 +1,10 @@
-export const SITE_TITLE = "Data in the Wild";
+export const SITE_TITLE =
+  "Data in the Wild - A Data Model Podcast brought to you by Xata";
 
-export const SITE_DESCRIPTION = "";
+export const SITE_DESCRIPTION =
+  "A podcast where your favorite indie hackers share their hard earned lessons and tall tales from their data model journey!";
 
-export const SHOW_TITLE =
-  '<em>DATA</em> <span>in the Wild</span>';
-
-// export const SHOW_DESCRIPTION =
-//   "A podcast where your favorite indie hackers share their data model journey so you can learn from their experience, both good and bad!";
+export const SHOW_TITLE = "<strong>DATA</strong> <em>in the Wild</em>";
 
 export const SHOW_DESCRIPTION =
-  "Learn from your favorite indie hackers as they share stories and tall tales from their data model journey!";
+  "Learn from your favorite indie hackers as they share hard earned lessons and <em>tall tales</em> from their data model journey!";
